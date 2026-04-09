5c73d5e7a92748fba9a715da864d6eefc13dad4f
Alexander M <niron_hsc@yahoo.com>
2026-04-10 01:41:20 +0300

    chore: Removing redundant usings

--- Lottery.Core/Models/DrawResult.cs
+++ Lottery.Core/Models/DrawResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lottery.Core.Models
+﻿namespace Lottery.Core.Models
 {
     public class DrawResult
     {
